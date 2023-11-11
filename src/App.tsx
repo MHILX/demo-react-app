c255441a8e4aad603b1f2c8fb3041803c5e73b9e
Mohammed Hoque <mohammed.hoque@intelex.com>
2023-11-10 22:31:08 -0600

    added counter component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { MyCounter } from '@mhilx/counter-component';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         Welcome
+        <MyCounter /> 
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
